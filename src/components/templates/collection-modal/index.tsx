461d73e04f5b9895416798668bacd8130892baeb
DaoQuangThiet <Thietdq1711@gmail.com>
2023-04-07 17:21:49 +0700

    Update index.tsx

--- src/components/templates/collection-modal/index.tsx
+++ src/components/templates/collection-modal/index.tsx
@@ -30,18 +30,18 @@ type CollectionModalFormData = {
   image3: unknown
   image2: unknown
   image4: unknown
+  icon1: unknown
+  icon2: unknown
+  icon3: unknown
+  icon4: unknown
   feature1: unknown
   feature2: unknown
   feature3: unknown
   feature4: unknown
-  feature5: unknown
-  feature6: unknown
   featureDesc1: unknown
   featureDesc2: unknown
   featureDesc3: unknown
   featureDesc4: unknown
-  featureDesc5: unknown
-  featureDesc6: unknown
   careinstructions: unknown
 }
 const CollectionModal: React.FC<CollectionModalProps> = ({
@@ -63,43 +63,33 @@ const CollectionModal: React.FC<CollectionModalProps> = ({
       name: "Key Feature No.1",
       featureTitle: "Title",
       featureDesc: "Description",
-      register: { ...register("feature1") },
-      register2: { ...register("featureDesc1") },
+      register1: { ...register("icon1") },
+      register2: { ...register("feature1") },
+      register3: { ...register("featureDesc1") },
     },
     {
       name: "Key Feature No.2",
       featureTitle: "Title",
       featureDesc: "Description",
-      register: { ...register("feature2") },
-      register2: { ...register("featureDesc2") },
+      register1: { ...register("icon2") },
+      register2: { ...register("feature2") },
+      register3: { ...register("featureDesc2") },
     },
     {
       name: "Key Feature No.3",
       featureTitle: "Title",
       featureDesc: "Description",
-      register: { ...register("feature3") },
-      register2: { ...register("featureDesc3") },
+      register1: { ...register("icon3") },
+      register2: { ...register("feature3") },
+      register3: { ...register("featureDesc3") },
     },
     {
       name: "Key Feature No.4",
       featureTitle: "Title",
       featureDesc: "Description",
-      register: { ...register("feature4") },
-      register2: { ...register("featureDesc4") },
-    },
-    {
-      name: "Key Feature No.5",
-      featureTitle: "Title",
-      featureDesc: "Description",
-      register: { ...register("feature5") },
-      register2: { ...register("featureDesc5") },
-    },
-    {
-      name: "Key Feature No.6",
-      featureTitle: "Title",
-      featureDesc: "Description",
-      register: { ...register("feature6") },
-      register2: { ...register("featureDesc6") },
+      register1: { ...register("icon4") },
+      register2: { ...register("feature4") },
+      register3: { ...register("featureDesc4") },
     },
   ]
   const imageList = [
@@ -140,18 +130,18 @@ const CollectionModal: React.FC<CollectionModalProps> = ({
         image2: collection.metadata.image2,
         image3: collection.metadata.image3,
         image4: collection.metadata.image4,
+        icon1: collection.metadata.icon1,
+        icon2: collection.metadata.icon2,
+        icon3: collection.metadata.icon3,
+        icon4: collection.metadata.icon4,
         feature1: collection.metadata.feature1,
         feature2: collection.metadata.feature2,
         feature3: collection.metadata.feature3,
         feature4: collection.metadata.feature4,
-        feature5: collection.metadata.feature5,
-        feature6: collection.metadata.feature6,
         featureDesc1: collection.metadata.featureDesc1,
         featureDesc2: collection.metadata.featureDesc2,
         featureDesc3: collection.metadata.featureDesc3,
         featureDesc4: collection.metadata.featureDesc4,
-        featureDesc5: collection.metadata.featureDesc5,
-        featureDesc6: collection.metadata.featureDesc6,
         careinstructions: collection.metadata.careinstructions,
       })
 
@@ -181,18 +171,18 @@ const CollectionModal: React.FC<CollectionModalProps> = ({
         (updateData["image2"] = data.image2),
         (updateData["image3"] = data.image3),
         (updateData["image4"] = data.image4),
+        (updateData["icon1"] = data.icon1),
+        (updateData["icon2"] = data.icon2),
+        (updateData["icon3"] = data.icon3),
+        (updateData["icon4"] = data.icon4),
         (updateData["feature1"] = data.feature1),
         (updateData["feature2"] = data.feature2),
         (updateData["feature3"] = data.feature3),
         (updateData["feature4"] = data.feature4),
-        (updateData["feature5"] = data.feature5),
-        (updateData["feature6"] = data.feature6),
         (updateData["featureDesc1"] = data.featureDesc1),
         (updateData["featureDesc2"] = data.featureDesc2),
         (updateData["featureDesc3"] = data.featureDesc3),
         (updateData["featureDesc4"] = data.featureDesc4),
-        (updateData["featureDesc5"] = data.featureDesc5),
-        (updateData["featureDesc6"] = data.featureDesc6),
         (updateData["careinstructions"] = data.careinstructions),
         update(
           {
@@ -229,12 +219,14 @@ const CollectionModal: React.FC<CollectionModalProps> = ({
         (createData["image2"] = data.image2 ? data.image2 : undefined),
         (createData["image3"] = data.image3 ? data.image3 : undefined),
         (createData["image4"] = data.image4 ? data.image4 : undefined),
+        (createData["icon1"] = data.icon1 ? data.icon1 : undefined),
+        (createData["icon2"] = data.icon2 ? data.icon2 : undefined),
+        (createData["icon3"] = data.icon3 ? data.icon3 : undefined),
+        (createData["icon4"] = data.icon4 ? data.icon4 : undefined),
         (createData["feature1"] = data.feature1 ? data.feature1 : undefined),
         (createData["feature2"] = data.feature2 ? data.feature2 : undefined),
         (createData["feature3"] = data.feature3 ? data.feature3 : undefined),
         (createData["feature4"] = data.feature4 ? data.feature4 : undefined),
-        (createData["feature5"] = data.feature5 ? data.feature5 : undefined),
-        (createData["feature6"] = data.feature6 ? data.feature6 : undefined),
         (createData["featureDesc1"] = data.featureDesc1
           ? data.featureDesc1
           : undefined),
@@ -246,12 +238,6 @@ const CollectionModal: React.FC<CollectionModalProps> = ({
           : undefined),
         (createData["featureDesc4"] = data.featureDesc4
           ? data.featureDesc4
-          : undefined),
-        (createData["featureDesc5"] = data.featureDesc5
-          ? data.featureDesc5
-          : undefined),
-        (createData["featureDesc6"] = data.featureDesc6
-          ? data.featureDesc6
           : undefined),
         (createData["careinstructions"] = data.careinstructions
           ? data.careinstructions
@@ -350,16 +336,26 @@ const CollectionModal: React.FC<CollectionModalProps> = ({
                     key={index}
                   >
                     <InputField
+                      label="Icon"
+                      placeholder="Icon Link"
+                      {...items.register1}
+                    />
+                  </div>
+                  <div
+                    className="mt-4 flex items-center gap-x-base"
+                    key={index}
+                  >
+                    <InputField
                       label={items.featureTitle}
                       placeholder="Title"
-                      {...items.register}
+                      {...items.register2}
                     />
                   </div>
                   <div className="mt-4">
                     <TextArea
                       label={items.featureDesc}
                       placeholder="Description"
-                      {...items.register2}
+                      {...items.register3}
                     />
                   </div>
                 </div>
